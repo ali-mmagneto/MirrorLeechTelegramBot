a73f0ba433f7afc08c794d15a90011701faffff9
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-09-24 22:11:40 +0300

    Update update.py

--- update.py
+++ update.py
@@ -44,12 +44,12 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/ali-mmagneto/MirrorLeechTelegramBot"
+    UPSTREAM_REPO = "https://github.com/ali-mmagneto/mirror-leech-telegram-bot"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
 except:
-    UPSTREAM_BRANCH = 'h-code'
+    UPSTREAM_BRANCH = 'master'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
