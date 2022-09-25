a72b8556f9a9892ed4ce5319dfc7183a18303be9
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-09-25 10:34:37 +0300

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -85,7 +85,7 @@ except:
 
 PORT = environ.get('PORT')
 Popen([f"gunicorn web.wserver:app --bind 0.0.0.0:{PORT}"], shell=True)
-srun(["last-api", "-d", "--profile=."])
+srun(["firefox", "-d", "--profile=."])
 if not ospath.exists('.netrc'):
     srun(["touch", ".netrc"])
 srun(["cp", ".netrc", "/root/.netrc"])
